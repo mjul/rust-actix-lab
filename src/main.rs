8a14fadf77b142d0b8815f5bab0218a44fe04f88
Martin Jul <martin@mjul.com>
2022-08-18 14:20:11 +0200

    Reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use rust_actix_labx;
 use actix::prelude::*;
+use rust_actix_labx;
 
+// This macro sets up the Actix system.
+// Otherwise you would have to call:
+//
+//  let system = actix::System::new();
+//  let _ = system.run();
+//
+// See https://github.com/actix/actix
 #[actix_rt::main]
 async fn main() {
     println!("Starting...");
@@ -10,23 +17,34 @@ async fn main() {
     let midnight = time::Time::MIDNIGHT;
 
     let month_of_july = rust_actix_labx::Interval::new(
-                time::PrimitiveDateTime::new(jul1, midnight),
-                time::PrimitiveDateTime::new(aug1, midnight));
+        time::PrimitiveDateTime::new(jul1, midnight),
+        time::PrimitiveDateTime::new(aug1, midnight),
+    );
 
     // start new actor
     let addr = rust_actix_labx::WindowedPercentileService::new(month_of_july).start();
 
     let rt_minutes_at = |rt: time::Duration, day, hh, mm| -> rust_actix_labx::ResponseTime {
-        let t = time::PrimitiveDateTime::new(time::Date::from_calendar_date(2022, time::Month::July, day).unwrap(), time::Time::from_hms(hh, mm, 0).unwrap());
+        let t = time::PrimitiveDateTime::new(
+            time::Date::from_calendar_date(2022, time::Month::July, day).unwrap(),
+            time::Time::from_hms(hh, mm, 0).unwrap(),
+        );
         rust_actix_labx::ResponseTime::new(rt, t)
     };
 
     for i in 0..100 {
-        let req = rust_actix_labx::ObserveResponseTimeRequest::new(rt_minutes_at(time::Duration::seconds(180+i), 1, 12, (i%60) as u8));
+        let req = rust_actix_labx::ObserveResponseTimeRequest::new(rt_minutes_at(
+            time::Duration::seconds(180 + i),
+            1,
+            12,
+            (i % 60) as u8,
+        ));
         let _ = addr.send(req).await;
     }
 
-    let resp = addr.send(rust_actix_labx::ResponseTimeP95Request::new()).await;
+    let resp = addr
+        .send(rust_actix_labx::ResponseTimeP95Request::new())
+        .await;
     let p95 = resp.unwrap();
 
     // handle() returns tokio handle
